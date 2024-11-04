f6f98e2ca25d21de4bb12c471ab81f12714afd93
Kaiser <minh.hackefr89@gmail.com>
2024-11-04 17:46:49 +0700

    config file

--- FrontEnd/src/containers/App.js
+++ FrontEnd/src/containers/App.js
@@ -15,7 +15,6 @@ import Login from "./Auth/Login";
 import Header from "./Header/Header";
 import System from "../routes/System";
 import { CustomToastCloseButton } from "../components/CustomToast";
-import ConfirmModal from "../components/ConfirmModal";
 
 class App extends Component {
   handlePersistorState = () => {
@@ -41,7 +40,6 @@ class App extends Component {
       <Fragment>
         <Router history={history}>
           <div className="main-container">
-            <ConfirmModal />
             {this.props.isLoggedIn && <Header />}
 
             <span className="content-container">
